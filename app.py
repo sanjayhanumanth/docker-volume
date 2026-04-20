3de785d9abeed6dec760f4e85e595676aafaf882
sanjayhanumanth <sanjayhanumanth.t@coherent.in>
2026-04-20 15:30:57 +0530

    log changes

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-import time
 import os
+import time
 
-file_path = "./data/log.txt"
+# Default to /data so it works with your --mount
+file_path = os.getenv("LOG_PATH", "/data/log.txt")
 
-# Create folder if not exists
-os.makedirs("./data", exist_ok=True)
+# Create directory if not exists
+os.makedirs(os.path.dirname(file_path), exist_ok=True)
 
 while True:
-    with open(file_path, "a") as f:
-        f.write("Hello from Docker Volume!\n")
-    print("Written to file...")
-    time.sleep(5)
+    try:
+        with open(file_path, "a") as f:
+            f.write("Hello from Docker Volume!\n")
+
+        print(f"Written to {file_path}", flush=True)
+        time.sleep(5)
+
+    except Exception as e:
+        print(f"Error: {e}", flush=True)
+        time.sleep(5)
